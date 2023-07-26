c3aa7e7a2c4d8f7faccc80604d28850f0bff6265
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-26 14:46:30 +0200

    improved menu

--- src/components/header/SelectProjectButton.tsx
+++ src/components/header/SelectProjectButton.tsx
@@ -1,21 +1,17 @@
-import { Popover } from '@headlessui/react'
+import { Menu } from '@headlessui/react'
 import { TProject } from "@/types/project";
 import { useRouter, usePathname } from "next/navigation";
 import navigation from "@/navigation";
 import type { Route } from "next";
-import { MdAddCircleOutline, MdUnfoldMore, MdCheck, MdSearch } from "react-icons/md";
+import { MdAddCircleOutline, MdUnfoldMore, MdCheck } from "react-icons/md";
 import Divider from '../core/Divider';
 import ProjectAvatar from './ProjectAvatar';
-import { useState } from 'react';
+import { Fragment } from 'react';
 
 export default function SelectProjectButton({ projects, project }: { projects: TProject[], project: TProject }) {
 
     const { push } = useRouter();
     const pathname = usePathname();
-
-    const [search, setSearch] = useState('');
-
-    const filteredProjects = search ? projects.filter(x => x.name.toLowerCase().includes(search.toLowerCase())) : projects;
 
     const selectProject = (projectId: string) => {
         const segments = pathname.split("/").filter(x => x !== "");
@@ -31,64 +27,58 @@ export default function SelectProjectButton({ projects, project }: { projects: T
     }
 
     return (
-        <Popover className="relative">
-            <Popover.Button className="h-10 w-7 rounded-md hover:bg-secondary-100 dark:hover:bg-secondary-800 flex items-center justify-center">
+        <Menu as="div" className="relative">
+            <Menu.Button className="h-10 w-7 rounded-md focus:bg-secondary-100 hover:bg-secondary-100 dark:focus:bg-secondary-800 dark:hover:bg-secondary-800 flex items-center justify-center">
                 <MdUnfoldMore className="h-6 w-6" />
-            </Popover.Button>
-            <Popover.Panel className="top-11 left-3.5 -translate-x-1/2 absolute z-10 rounded-md overflow-hidden shadow border border-secondary-300 bg-white dark:border-secondary-700 dark:bg-secondary-800">
-                {({ close }) => (
-                    <>
-                        <div className="p-4">
-                            <h2 className="text-sm font-medium">
-                                Projects
-                            </h2>
-                        </div>
-                        <Divider />
-                        <div className="px-4 gap-3 flex items-center justify-center" >
-                            <MdSearch className="h-5 w-5 text-secondary-400" />
-                            <input
-                                autoFocus
-                                className="py-4 w-full text-sm dark:bg-secondary-800 placeholder-secondary-400"
-                                placeholder={'Find Project\u2026'}
-                                value={search}
-                                onChange={e => setSearch(e.target.value)}
-                            >
-                            </input>
-                        </div>
-                        <Divider />
-                        <div className="py-2 overflow-auto w-[280px] max-h-[200px]">
-                            <ul>
-                                {filteredProjects.map(x => (
-                                    <li
-                                        key={x.id}
-                                        className="flex items-center px-4 py-2 gap-3 text-sm cursor-pointer hover:bg-secondary-100 dark:hover:bg-secondary-700"
-                                        onClick={() => { close(); setSearch(""); selectProject(x.id); }}
-                                    >
-                                        <ProjectAvatar project={x} />
-                                        <span className="flex-1 whitespace-nowrap overflow-hidden text-ellipsis">
-                                            {x.name}
-                                        </span>
-                                        <div className="w-5 flex items-center justify-end">
-                                            {project.id === x.id && <MdCheck className='h-5 w-5' />}
-                                        </div>
-                                    </li>
-                                ))}
-                                {filteredProjects.length === 0 && <li className="px-4 py-2 text-sm text-secondary-400">No projects found</li>}
-                            </ul>
-                        </div>
-                        <Divider />
-                        <div className="py-2">
-                            <ul>
-                                <li className="flex items-center px-4 py-2 gap-3 text-sm cursor-pointer text-secondary-700 dark:text-secondary-300 hover:bg-secondary-100 dark:hover:bg-secondary-700"
-                                    onClick={() => { close(); setSearch(""); push(`${navigation.newProject}?backUrl=${encodeURIComponent(pathname)}`) }}
+            </Menu.Button>
+            <Menu.Items className="top-11 left-3.5 -translate-x-1/2 absolute z-10 rounded-md overflow-hidden shadow border border-secondary-300 bg-white dark:border-secondary-700 dark:bg-secondary-800">
+                <div className="p-4">
+                    <h2 className="text-sm font-medium">
+                        Projects
+                    </h2>
+                </div>
+                <Divider />
+                <div className="py-2 overflow-auto w-[280px] max-h-[200px]">
+                    {projects.map(x => (
+                        <Menu.Item key={x.id} as={Fragment} >
+                            {({ active }) => (
+                                <button
+                                    className={active ?
+                                        "w-full flex items-center px-4 py-2 gap-3 text-sm cursor-pointer bg-secondary-100 dark:bg-secondary-700" :
+                                        "w-full flex items-center px-4 py-2 gap-3 text-sm cursor-pointer hover:bg-secondary-100 dark:hover:bg-secondary-700"
+                                    }
+                                    onClick={() => selectProject(x.id)}
                                 >
-                                    <MdAddCircleOutline className="h-5 w-5 text-primary-500 dark:text-primary-400" />
-                                    Create Project
-                                </li>
-                            </ul>
-                        </div>
-                    </>)}
-            </Popover.Panel>
-        </Popover>
+                                    <ProjectAvatar project={x} />
+                                    <span className="flex-1 text-left whitespace-nowrap overflow-hidden text-ellipsis">
+                                        {x.name}
+                                    </span>
+                                    <div className="w-5 flex items-center justify-end">
+                                        {project.id === x.id && <MdCheck className='h-5 w-5' />}
+                                    </div>
+                                </button>
+                            )}
+                        </Menu.Item>
+                    ))}
+                </div>
+                <Divider />
+                <div className="py-2">
+                    <Menu.Item as={Fragment}>
+                        {({ active }) => (
+                            <button
+                                className={active ?
+                                    "w-full flex items-center px-4 py-2 gap-3 text-sm cursor-pointer text-secondary-700 dark:text-secondary-300 bg-secondary-100 dark:bg-secondary-700" :
+                                    "w-full flex items-center px-4 py-2 gap-3 text-sm cursor-pointer text-secondary-700 dark:text-secondary-300 hover:bg-secondary-100 dark:hover:bg-secondary-700"
+                                }
+                                onClick={() => push(`${navigation.newProject}?backUrl=${encodeURIComponent(pathname)}`)}
+                            >
+                                <MdAddCircleOutline className="h-5 w-5 text-primary-500 dark:text-primary-400" />
+                                Create Project
+                            </button>
+                        )}
+                    </Menu.Item>
+                </div>
+            </Menu.Items>
+        </Menu>
     )
 }
