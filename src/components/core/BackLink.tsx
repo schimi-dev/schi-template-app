5a763a795b7c33cae44625811cd01bed9163c36b
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-26 22:00:22 +0200

    colors

--- src/components/core/BackLink.tsx
+++ src/components/core/BackLink.tsx
@@ -8,7 +8,7 @@ export default function BackLink<T extends string>({ href, children }: { href: R
     return (
         <Link
             href={href}
-            className="flex items-center justify-center text-sm font-medium text-secondary-400  px-3 py-2 rounded-md hover:bg-secondary-100 dark:hover:bg-secondary-800"
+            className="flex items-center justify-center text-sm font-medium text-secondary-500 dark:text-secondary-400 px-3 py-2 rounded-md hover:bg-secondary-100 dark:hover:bg-secondary-800"
         >
             <MdNavigateBefore className="h-4 w-4 -ml-1" />
             {children || "Back"}
