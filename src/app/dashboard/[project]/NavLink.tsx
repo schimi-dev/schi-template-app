3f85b80f3cd9042ec23aa192e0f186896f748a97
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-26 21:52:32 +0200

    client

---
+++ src/app/dashboard/[project]/NavLink.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import type { Route } from "next"
+import { ReactNode } from "react"
+
+export default function NavLink<T extends string>({ href, children }: { href: Route<T>, children: ReactNode }) {
+
+    const pathname = usePathname()
+
+    const active = pathname === href
+
+    return (
+        <Link 
+        href={href} 
+        className={active?
+            "text-sm flex items-center gap-3 px-5 py-2 font-medium bg-secondary-100 dark:bg-secondary-800" :
+            "text-sm flex items-center gap-3 px-5 py-2 font-normal hover:bg-secondary-100 dark:hover:bg-secondary-800  text-secondary-700 dark:text-secondary-300"
+        }
+            >
+            {children}
+        </Link>
+    )
+
+}
--- src/app/dashboard/[project]/layout.tsx
+++ src/app/dashboard/[project]/layout.tsx
@@ -1,26 +1,29 @@
 import navigation from "@/navigation";
-import Link from "next/link";
 import { ReactNode } from "react";
+import NavLink from "./NavLink";
+import { MdOutlineDashboard, MdOutlineSettings } from "react-icons/md";
 
 export default function Layout({ params, children }: { params: { project: string }, children: ReactNode }) {
 
     return (
         <div>
-            <nav className="fixed left-0 top-14 border-r border-r-secondary-300 dark:border-r-secondary-700 h-[calc(100vh-56px)] w-60 overflow-auto">
-                <ul className="flex flex-col gap-2 p-5">
+            <nav className="fixed left-0 top-14 border-r border-r-secondary-300 dark:border-r-secondary-700 h-[calc(100vh-56px)] w-56 overflow-auto">
+                <ul className="flex flex-col py-4">
                     <li>
-                        <Link href={navigation.singleProjectOverview(params.project)}>
+                        <NavLink href={navigation.singleProjectOverview(params.project)}>
+                            <MdOutlineDashboard className="h-4 w-4" />
                             Overview
-                        </Link>
+                        </NavLink>
                     </li>
                     <li>
-                        <Link href={navigation.singleProjectSettings(params.project)}>
+                        <NavLink href={navigation.singleProjectSettings(params.project)}>
+                            <MdOutlineSettings className="h-4 w-4" />
                             Settings
-                        </Link>
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
-            <div className="w-[calc(100%-240px)] ml-60">
+            <div className="w-[calc(100%-224px)] ml-56">
                 {children}
             </div>
         </div>
