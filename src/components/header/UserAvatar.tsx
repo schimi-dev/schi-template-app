3f0c36619f044df82bd9953dbfcb15266ee679c3
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-28 15:23:06 +0200

    refined user avatar

--- src/components/header/UserAvatar.tsx
+++ src/components/header/UserAvatar.tsx
@@ -7,7 +7,7 @@ export default function UserAvatar({ user }: { user: NonNullable<Session["user"]
         <Image
             className="rounded-full border border-secondary-300 dark:border-secondary-700"
             src={"/avatars/preset.svg"}
-            alt={user.name || "Avatar"}
+            alt={user.name ?? "Avatar"}
             height={32}
             width={32}
         />
