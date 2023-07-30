24dc3271f68108f1164a44b69fd96aa81de53191
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-30 12:45:41 +0200

    documented returning null inside components

--- src/components/header/ProjectLink.tsx
+++ src/components/header/ProjectLink.tsx
@@ -11,6 +11,12 @@ export default function ProjectLink({ projects }: { projects: TProject[] }) {
     const projectParam = useProjectParam();
     const project = projectParam ? projects.find(project => project.id === projectParam) : null;
 
+    // Returning `null` is a suitable approach because functionality that is only available in Client Components is needed to detect if this component should be rendered.
+    // That way, the header of the app can remain a Server Component and this component can detect based on the dynamic params if it should render anything.
+    // Moreover, this component is intended to be only used in the header of the app which mitigates the fact that returning `null` might surprise a developer trying to render it.
+    // https://legacy.reactjs.org/docs/conditional-rendering.html#preventing-component-from-rendering
+    // https://react.dev/learn/conditional-rendering#conditionally-returning-nothing-with-null
+    // https://nextjs.org/docs/getting-started/react-essentials#moving-client-components-to-the-leaves
     if (!project)
         return null;
 
--- src/components/header/Seperator.tsx
+++ src/components/header/Seperator.tsx
@@ -5,6 +5,12 @@ import useProjectParam from "./useProjectParam";
 export default function Seperator() {
     const projectParam = useProjectParam();
 
+    // Returning `null` is a suitable approach because functionality that is only available in Client Components is needed to detect if this component should be rendered.
+    // That way, the header of the app can remain a Server Component and this component can detect based on the dynamic params if it should render anything.
+    // Moreover, this component is intended to be only used in the header of the app which mitigates the fact that returning `null` might surprise a developer trying to render it.
+    // https://legacy.reactjs.org/docs/conditional-rendering.html#preventing-component-from-rendering
+    // https://react.dev/learn/conditional-rendering#conditionally-returning-nothing-with-null
+    // https://nextjs.org/docs/getting-started/react-essentials#moving-client-components-to-the-leaves
     if (!projectParam)
         return null;
 
